8b9a384a3afb4e4710dccf8d192d39c2c53940f4
Natacha Vergara <ntchvergara@gmail.com>
2022-02-28 23:56:49 -0300

    agregando notas al js

--- js/index.js
+++ js/index.js
@@ -15,20 +15,21 @@ let pronostico = document.querySelector('#pronostico')
 let humedad = document.querySelector('#humedad')
 
 let viento = document.querySelector('#viento')
-
+//creo una variable coon mi api, de esta forma se lee mejor en la ruta
 const api = `a89dac52a766017467944b8f47420333`;
 
 
 
-
+//creo una funcion que al hacer click en buscar me va a realizar una busqueda por api 
 
 const peticionClima = () => {
     btn.addEventListener('click', () => {
        
-
+        //dentro de la url ingreso la variable que el usuario ingresa en el campo de ciudad  y ademas la api key
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputCiudad.value}&units=metric&appid=${api}&lang=sp`)
             .then(res => res.json())
             .then(data => {
+                //con el resultado de la data empiezo a ingresar los datos en mi innerHTML
                 console.log(data)
                 textoCiudad.innerHTML = data.name
                 pronostico.innerHTML = data.weather[0].description
@@ -43,7 +44,7 @@ const peticionClima = () => {
             })
             .catch(err => console.log(err))
         
-        
+        //borro el campo de busqueda
         inputCiudad.value = ""
         
         
